d5bde45bc1063f85bdb69a9ccb01342fcb8b55e8
Asif-102 <asif1zero2@gmail.com>
2022-12-13 01:38:08 +0600

    insert info with proper info

--- log.c
+++ log.c
@@ -7,7 +7,7 @@
 
 int main() {
     time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
+    struct tm *tm = localtime(&t);
     FILE* logFile = fopen("log.txt", "a");
     FILE* input = fopen("input.txt", "r");
     if (input == NULL) {
@@ -18,7 +18,7 @@ int main() {
         char ch = fgetc(input);
         if (ch == EOF)
             break;
-        fprintf(logFile, "%c -> now: %d-%02d-%02d %02d:%02d:%02d\n", ch, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        fprintf(logFile, "%c %s", ch, asctime(tm));
     }
     return 0;
 }
