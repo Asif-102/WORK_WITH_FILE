4088ec963210830ccf276bc0f97f7666066d7675
Asif-102 <asif1zero2@gmail.com>
2022-12-13 01:05:35 +0600

    input output from file

---
+++ ph.c
@@ -0,0 +1,21 @@
+/**
+* author: asif_102
+* created: 2022-12-13 00:52:12
+**/
+#include <stdio.h>
+
+int main() {
+    FILE* inputFile = fopen("input.txt", "r");
+    FILE* outputFile = fopen("output.txt", "w");
+    if(inputFile == NULL || outputFile == NULL){
+        printf("file not found...");
+        return 0;
+    }
+    while (1) {
+        char ch = fgetc(inputFile);
+        if(ch == EOF)
+            break;
+        fputc(ch, outputFile);
+    }
+    return 0;
+}
